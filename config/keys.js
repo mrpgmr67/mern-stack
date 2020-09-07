dd26861feefffafee7a283e108d3c4b8142c3c1a
Michael Pomatto <mike@pomatto.com>
2020-09-07 18:58:10 -0400

    Back End Completed

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //mongoURI: 'mongodb://'shopping-list-user:Underground1@cluster0.x77ty.mongodb.net:27017/BASE
+    mongoURI: "mongodb+srv://shopping-list-user:Underground1@cluster0.x77ty.mongodb.net/BASE?retryWrites=true&w=majority"
+}
+
