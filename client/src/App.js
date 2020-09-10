a8981958a493282eaf203232bed2465c7eaa099a
Michael Pomatto <mike@pomatto.com>
2020-09-10 17:15:00 -0400

    Updated with List

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import AppNavbar from './components/AppNavbar'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
