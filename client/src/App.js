ce195bc2d8e12b4f62c2c8ccd6cd76389e5e9c34
Michael Pomatto <mike@pomatto.com>
2020-09-13 01:11:55 -0400

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
+    <Provider store = { store }>
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 
